98d0d04b66fd827c775860491f6b336b5976eaa8
calm1205 <naginani072@gmail.com>
2022-06-11 10:47:30 +0900

    hash create

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,48 @@
-console.log("hello world");
+type BlockType = {
+  timestamp: Date;
+  transactions: string[];
+  nonce: number;
+  previousHash: string;
+};
+
+class BlockChain {
+  private _transactionPool: string[];
+  private _chain: BlockType[];
+
+  constructor(nonce?: number, previousHash?: string) {
+    this._transactionPool = [];
+    this._chain = [];
+    this.createBlock(0, "init hash");
+  }
+
+  get chain() {
+    return this._chain;
+  }
+
+  public createBlock = (nonce: number, previousHash: string) => {
+    const block = {
+      timestamp: new Date(),
+      transactions: this._transactionPool,
+      nonce,
+      previousHash,
+    };
+    this.chain.push(block);
+
+    return block;
+  };
+}
+
+function log(chains: BlockType[]) {
+  chains.forEach((c, i) => {
+    console.log("_".repeat(15), "Chain", i, "_".repeat(15));
+    console.table(c);
+  });
+  console.log("\n\n\n");
+}
+
+const blockChain = new BlockChain();
+log(blockChain.chain);
+blockChain.createBlock(5, "hash 1");
+log(blockChain.chain);
+blockChain.createBlock(6, "hash 3");
+log(blockChain.chain);
